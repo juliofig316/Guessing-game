4281fca06332ba58a3a24f51873591bc8d02658a
juliofig316 <99454684+juliofig316@users.noreply.github.com>
2022-07-16 22:12:26 -0400

    Add files via upload
    Added conditional statement to see if number is less or more than 1-100

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const theNumber = Math.floor((Math.random() * 100) + 1);
 
 const userInput = document.getElementById('guess');
 
@@ -6,31 +5,36 @@ const submit = document.getElementById('submit');
 
 const guesses = document.getElementsByTagName('li');
 
+const inputFeedback = document.getElementById('feedback');
+
+const theNumber = Math.floor(Math.random() * 100) + 1;
+
+
 listItemIdx = 0;
 
 submit.addEventListener('click', function(event) {
+    if (userInput.value < 1 || userInput.value > 100){
+        alert("Please enter number from 1 to 100")
+    } else {
     guesses[listItemIdx].innerText = userInput.value;
     listItemIdx++;
     userInput.value = '';
+    }
 });
 
-let inputFeedback = document.getElementById('feedback');
-    if (userInput > theNumber) {
-        inputFeedback.innerText("Too high")
-    } else if (userInput < theNumber) {
-        inputFeedback.innerText("Too low") 
-    } else if (userInput === theNumber) {
+submit.addEventListener("Click", function(event){
+    if (userInput.value === theNumber) {
         inputFeedback.innerText("You guessed correctly!")
-    }
+    } else if (userInput.value < theNumber) {
+        inputFeedback.innerText("You guessed too low!") 
+    } else if (userInput.value > theNumber) {
+        inputFeedback.innerText("You guessed too high!")
+        }
+    });
+
+// restart the game from 0
+
+//  const reset = document.getElementById("again");
 
 
-
-
-
-// Adds guesses to UL's
-
-// indicator that says how close you are
-
-// hint button and reset game are bonuses?
-
-// 
+// hint button
