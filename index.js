92cba20320d15d00f6cb758eb644a8363c2bbf15
juliofig316 <99454684+juliofig316@users.noreply.github.com>
2022-07-17 19:40:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,38 +5,34 @@ const submit = document.getElementById('submit');
 
 const guesses = document.getElementsByTagName('li');
 
-const inputFeedback = document.getElementsByTagName('h2');
+const inputFeedback = document.getElementById('feedback');
 
-const theNumber = Math.floor(Math.random() * 100) + 1;
+var theNumber = Math.floor(Math.random() * 100);
+
+const playAgain = document.getElementById('again');
 
 
 listItemIdx = 0;
 
 submit.addEventListener('click', function(event) {
-    if (userInput.value < 1 || userInput.value > 100){
+    if (userInput.value < 1 || userInput.value > 101){
         alert("Please enter number from 1 to 100")
     } else {
+
     guesses[listItemIdx].innerText = userInput.value;
     listItemIdx++;
     userInput.value = '';
     }
 });
 
-submit.addEventListener("Click", function(event){
+submit.addEventListener("click", function (event) {
     var userInput = document.getElementById('guess');
-    var inputFeedback = document.getElementsByTagName('h2');
+    var inputFeedback = document.getElementById('feedback');
     if (userInput.value === theNumber) {
-        inputFeedback.innerText("You guessed correctly!")
+        inputFeedback.innerText = "You guessed correctly!"
     } else if (userInput.value < theNumber) {
-        inputFeedback.innerText("You guessed too low!") 
+        inputFeedback.innerText = "You guessed too low!"
     } else if (userInput.value > theNumber) {
-        inputFeedback.innerText("You guessed too high!")
+        inputFeedback.innerText = "You guessed too high!"
     }
-    });
-
-// restart the game from 0
-
-//  const reset = document.getElementById("again");
-
-
-// hint button
+});
